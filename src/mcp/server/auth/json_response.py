f788d7900beed7fa0ae582c9e855d6b35b664af5
Peter Raboud <praboud@anthropic.com>
2025-03-11 08:15:16 -0700

    Add json_response.py comment

--- src/mcp/server/auth/json_response.py
+++ src/mcp/server/auth/json_response.py
@@ -4,5 +4,7 @@ from starlette.responses import JSONResponse
 
 
 class PydanticJSONResponse(JSONResponse):
+    # use pydantic json serialization instead of the stock `json.dumps`,
+    # so that we can handle serializing pydantic models like AnyHttpUrl
     def render(self, content: Any) -> bytes:
         return content.model_dump_json(exclude_none=True).encode("utf-8")
