9bee1d97fe4c0f8b626679bf98ce8be343404511
AishwaryaKalloli <30429206+AishwaryaKalloli@users.noreply.github.com>
2025-05-12 23:21:43 +0530

    Test fixes for v1.7.x (#684)

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -69,8 +69,14 @@ async def test_desktop(monkeypatch):
         content = result.contents[0]
         assert isinstance(content, TextResourceContents)
         assert isinstance(content.text, str)
-        assert "/fake/path/file1.txt" in content.text
-        assert "/fake/path/file2.txt" in content.text
+        assert any(
+            path in content.text
+            for path in ["/fake/path/file1.txt", "\\\\fake\\\\path\\\\file1.txt"]
+        )
+        assert any(
+            path in content.text
+            for path in ["/fake/path/file2.txt", "\\\\fake\\\\path\\\\file2.txt"]
+        )
 
 
 @pytest.mark.parametrize("example", find_examples("README.md"), ids=str)
