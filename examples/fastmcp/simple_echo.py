98129c25906142178908be6ae6813fa5161d65d1
文徐 <wangwenxue.wwx@antgroup.com>
2025-07-31 22:49:37 +0800

    recover unused change

--- examples/fastmcp/simple_echo.py
+++ examples/fastmcp/simple_echo.py
@@ -2,16 +2,13 @@
 FastMCP Echo Server
 """
 
-from mcp.server.fastmcp import FastMCP,Context
+from mcp.server.fastmcp import FastMCP
 
 # Create server
 mcp = FastMCP("Echo Server")
 
 
 @mcp.tool()
-def echo(text: str, ctx: Context) -> str:
+def echo(text: str) -> str:
     """Echo the input text"""
-    ctx.request_context.request.query_params.get("session_id")
     return text
-
-mcp.run(transport="sse")
